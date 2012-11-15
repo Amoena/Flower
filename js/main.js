cde8b04450f2816f298050c247236d301dd563a1
Amoena <angrygingerbread@gmail.com>
2012-11-16 01:22:00 +0300

    navigation menu : hover effects added

--- js/main.js
+++ js/main.js
@@ -13,4 +13,33 @@ $("#articles img").hover(
         width: '-=10px'
         },50)
     });
+    //---------- hover on navigation
+    $(".nav li .home").hover(
+        function () {
+            $(this).css('background-position', '-241px 0px');
+            $(this).css('color', '#b1a7aa');
+        },
+        function () {
+            $(this).css('background-position', '0px 0px');
+            $(this).css('color', '#2c2829');
+        });
+    $(".nav li .info").hover(
+        function () {
+            $(this).css('background-position', '-241px -141px');
+            $(this).css('color', '#b1a7aa');
+        },
+        function () {
+            $(this).css('background-position', '0px -141px');
+            $(this).css('color', '#2c2829');
+        });
+    $(".nav li .contacts").hover(
+        function () {
+            $(this).css('background-position', '-241px -282px');
+            $(this).css('color', '#b1a7aa');
+        },
+        function () {
+            $(this).css('background-position', '0px -282px');
+            $(this).css('color', '#2c2829');
+        });
+
 });
